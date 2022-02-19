4152e331ec0e1f454e9030612fbe7889bc0e0f34
chuongduong <duongthuychuong@gmail.com>
2022-02-19 21:11:43 +0700

    change env path

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+require('dotenv').config({
+    path: `${__dirname}/.env`
+})
 const mongoose = require('mongoose')
 const express = require('express')
 const app = express()
